2eaf061f01f0bc2a5bb1cb66668168890e2700c0
Abdullah <alkhamsi.abdullah@gmail.com>
2018-12-13 22:43:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
 
 client.on('ready', () => {
   // Get the channel via ID
-  let channel = client.channels.get('522355021069811712');
+  let channel = client.channels.get('522860286617845760');
   channel.join()
   .then(connection => console.log('Connected'))
   .catch(console.error);
@@ -47,6 +47,9 @@ const queue = new Map();
  
 var prefix = "2" 
 client.on('message', async msg => {
+    //--
+    if (msg.channel.id === process.env.BOT_CHAT_ID) {
+ 
     if (msg.author.bot) return undefined;
    
     if (!msg.content.startsWith(prefix)) return undefined;
@@ -171,7 +174,8 @@ ${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
     }
  
     return undefined;
-});
+    }
+}); //--
  
 async function handleVideo(video, msg, voiceChannel, playlist = false) {
     const serverQueue = queue.get(msg.guild.id);
@@ -237,8 +241,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const adminprefix = "1admin";
+const devs = ['482335925867642881'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
